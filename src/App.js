a9890caa1ae9d1ef5dfbdd7a21c7c955922d0cb8
DotLasher001 <94836010+Vidhanvyrs@users.noreply.github.com>
2023-08-13 23:03:04 +0530

    Update App.js
    removed unused variables that were assigned the values but never used

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Main from "./Main";
 
 function App() {
   const [data, setData] = useState([]);
-  const [locationName, setLocationName] = useState("");
-  const [location, setlocation] = useState("");
-  const [searchClicked, setSearchClicked] = useState(false);
-  const [getUpdates, setGetUpdates] = useState(false);
+  const [location, setLocation] = useState("");
   const [mapCoordinates, setMapCoordinates] = useState({ lat: "", lon: "" });
 
   useEffect(() => {
@@ -28,27 +25,22 @@ function App() {
     }
   }, [data]);
 
-  const searchBtn = (e)=>{
-    if(e.key == "Enter"){
-      searchLocation()
+  const searchBtn = (e) => {
+    if (e.key === "Enter") {
+      searchLocation();
     }
-  }
-  
-  const searchLocation = (event) => {
+  };
+
+  const searchLocation = () => {
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=50c7eef00d13b49fb4b8a0b1dbaceafb`;
 
     axios.get(url).then((response) => {
       setData(response.data);
-      setSearchClicked(true);
     });
   };
 
   const locator = (event) => {
-    setlocation(event.target.value);
-  };
-
-  const updateHandler = () => {
-    setGetUpdates(true);
+    setLocation(event.target.value);
   };
 
   return (
@@ -62,20 +54,29 @@ function App() {
         <link
           href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Amatic+SC:wght@700&family=Anton&family=Bangers&family=Barlow+Condensed:wght@100&family=Bungee+Spice&family=Climate+Crisis&family=Inspiration&family=Modak&family=Press+Start+2P&family=Shadows+Into+Light&family=Silkscreen&family=Smokum&family=Special+Elite&family=Vina+Sans&display=swap"
           rel="stylesheet"
-        ></link>
+        />
         <div className="search">
           <button className="cc">
-            <a href="https://www.un.org/en/climatechange" target="_blank">
+            <a
+              href="https://www.un.org/en/climatechange"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               🌎
             </a>
           </button>
           <span className="info-box">Know more about Climate Change</span>
-          <Input val={location} onchange={locator} btn={searchLocation} btn2={searchBtn}/>
+          <Input
+            val={location}
+            onchange={locator}
+            btn={searchLocation}
+            btn2={searchBtn}
+          />
           <div className="updater">
             <Main location={location} />
             <Map lat={mapCoordinates.lat} lon={mapCoordinates.lon} />
           </div>
-          <footer>Made with ❤ for Nature&People</footer>
+          <footer>Made with ❤ for Nature & People</footer>
         </div>
       </div>
     </AuthContext.Provider>
